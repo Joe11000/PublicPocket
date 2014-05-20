c9c551054d96800b1553b567b78d6c81504ab6a6
Joe <JoeNoonan27@gmail.com>
2014-05-20 09:32:50 -0500

    initial stuffings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CorsTest::Application.config.secret_key_base = '108f2bbe14f0353924b5006aa4cec2c7ff2785f45ec14701b2cb47c16c4a8e21f35dfc591af580704ef43f8fc6ef0b6f73f553325196304d7833594fc94d0a25'
