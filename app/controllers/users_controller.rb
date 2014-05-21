26dfb5940aac2710bb13162d060f35d2bf27892b
Joe <JoeNoonan27@gmail.com>
2014-05-21 08:43:53 -0500

    add response header in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	def new
+		response.headers['Access-Control-Allow-Origin'] = '*'
 		render text: "It worked from #{params[:location]}" and return
 	end
 end
