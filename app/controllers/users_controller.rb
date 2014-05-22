736d06727df4b87d6d94146ad112aed9775b1b85
Joe <JoeNoonan27@gmail.com>
2014-05-22 16:27:07 -0500

    I think I got it FINALLY

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
-	before_filter :set_headers
+	before_filter :cors_preflight_check
+	after_filter :cors_set_access_control_headers if :options
 
 	def new
-
-		render text: params['location'] and return
+		debugger
+		render text: "one #{params}" and return
 	end
 
 	def options
+		debugger
+		render text: "two #{params['location']}" and return
+	end
 
+  def create
+		debugger
+		render text: "You are on #{params['location']}" and return
 	end
 
   private
@@ -20,12 +27,76 @@ class UsersController < ApplicationController
     headers['Access-Control-Max-Age'] = '86400'
   end
 
+# end
+
+
+	# For all responses in this controller, return the CORS access control headers.
+
+	def cors_set_access_control_headers
+	  headers['Access-Control-Allow-Origin'] = '*'
+	  headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+	  headers['Access-Control-Max-Age'] = "1728000"
+	end
+
+	# If this is a preflight OPTIONS request, then short-circuit the
+	# request, return only the necessary headers and return an empty
+	# text/plain.
+
+	def cors_preflight_check
+	  if request.method == :options
+	    headers['Access-Control-Allow-Origin'] = '*'
+	    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+	    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+	    headers['Access-Control-Max-Age'] = '1728000'
+	    render :text => '', :content_type => 'text/plain'
+	  end
+	end
 end
 
+
+
+
+# var data = new FormData();
+# data.append('location', window.location.toString())
+
+# var url = "http://localhost:3000/users";
 # var xhr = new XMLHttpRequest();
-# xhr.open("get", "http://cors-test-101.herokuapp.com/users/new", true);
+# xhr.open("post", url, true);
 # xhr.onload = function(info){console.log(info)}
-# xhr.send({'location': "http://www.video.nhl.com"})
+# xhr.send(data)
 
 
-# xhr.setRequestHeader('Access-Control-Allow-Origin','*')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# THIS WORKS TO SEND THIS CONTROLLER STUFF
+# (function(){
+#   var newscript = document.createElement('script');
+#      newscript.type = 'text/javascript';
+#      newscript.async = true;
+#      newscript.src = 'http://code.jquery.com/jquery-latest.min.js';
+#   (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+# })();
+
+
+
+# here = window.location.toString()
+# a = $.ajax({
+#   type: "post",
+#   url: "http://localhost:3000/users",
+#   data: {'location': here},
+#   crossDomain: true,
+#   xhrFields: {
+#     withCredentials: true
+#   }
+# })
--- config/application.rb
+++ config/application.rb
@@ -17,23 +17,23 @@ module CorsTest
 ############
     require 'rack/cors'
     config.middleware.use Rack::Cors do
-      allow do
-        origins 'localhost:3000', '127.0.0.1:3000',
-                /http:\/\/192\.168\.0\.\d{1,3}(:\d+)?/
-                # regular expressions can be used here
+      # allow do
+      #   origins 'localhost:3000', '127.0.0.1:3000',
+      #           /http:\/\/192\.168\.0\.\d{1,3}(:\d+)?/
+      #           # regular expressions can be used here
 
-        resource '/file/list_all/', :headers => 'x-domain-token'
-        resource '/file/at/*',
-            :methods => [:get, :post, :put, :delete, :options],
-            :headers => 'x-domain-token',
-            :expose  => ['Some-Custom-Response-Header'],
-            :max_age => 600
-            # headers to expose
-      end
+      #   resource '/file/list_all/', :headers => 'x-domain-token'
+      #   resource '/file/at/*',
+      #       :methods => [:get, :post, :put, :delete, :options],
+      #       :headers => 'x-domain-token',
+      #       :expose  => ['Some-Custom-Response-Header'],
+      #       :max_age => 600
+      #       # headers to expose
+      # end
 
       allow do
         origins '*'
-        resource '/public/*', :headers => :any, :methods => :get
+        resource '*', :headers => :any, :methods => [ :get, :post, :options ]
       end
     end
 ############# config for Cross Origin Resource Sharing gem
