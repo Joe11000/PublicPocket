194e9290086f858e0ec3660c44401659b6fe06e4
Joe <JoeNoonan27@gmail.com>
2014-05-22 13:35:46 -0500

    added root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
 	def new
 		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.headers['X-Frame_Options'] = ""
 		render text: "It worked from #{params[:location]}" and return
 	end
 end
+
+
+# var xhr = new XMLHttpRequest();
+# xhr.open("get", "http://pure-ravine-3133.herokuapp.com/users/new", true);
+# xhr.setRequestHeader('Access-Control-Allow-Origin','*')
+# xhr.onload = function(info){console.log(info)}
+# xhr.send({'location': "http://www.video.nhl.com"})
