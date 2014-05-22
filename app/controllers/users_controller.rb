41d8b9cb07260bb0060cb0f9ef80e41f68f9d927
Joe <JoeNoonan27@gmail.com>
2014-05-22 14:31:16 -0500

    should work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,30 @@
 class UsersController < ApplicationController
+	before_filter :set_headers
+
 	def new
-		response.headers['Access-Control-Allow-Origin'] = '*'
-		response.headers['X-Frame_Options'] = ""
 		render text: "It worked from #{params[:location]}" and return
 	end
-end
 
+	def options
+
+	end
+
+  private
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+    headers['Access-Control-Max-Age'] = '86400'
+  end
+
+end
 
 # var xhr = new XMLHttpRequest();
 # xhr.open("get", "http://pure-ravine-3133.herokuapp.com/users/new", true);
-# xhr.setRequestHeader('Access-Control-Allow-Origin','*')
 # xhr.onload = function(info){console.log(info)}
 # xhr.send({'location': "http://www.video.nhl.com"})
+
+
+# xhr.setRequestHeader('Access-Control-Allow-Origin','*')
