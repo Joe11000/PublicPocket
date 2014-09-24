56aa29cefe75a693b7ed31ba016cb86547bf1c58
Joe Noonan <joenoonan27@gmail.com>
2014-09-24 17:49:14 -0500

    Hides tables cooresponding to the names in the side column

--- app/assets/javascripts/sites_side_column.js
+++ app/assets/javascripts/sites_side_column.js
@@ -1,4 +1,6 @@
 $(function(){
+
+  { // #side_col has fixed positioning on large; else normal bootstrap positioning
     var pxl_lg = 1170;
 
     // initial setting
@@ -23,4 +25,26 @@ $(function(){
         $("#side_col").addClass("lg-fixed-pos")
       }
     });
+  }
+
+
+  { // toggle .hide on .list-group-item on click to change visibility
+    $(".list-group-item").each(function()
+    {
+      $(this).click(function(){
+        if($(this).hasClass("active"))
+        {
+          $(this).removeClass("active");                                 // deselect the side_col item
+          id = "#" + this.text.match(/\W*(\w+)/)[1].toLowerCase() + "_table"   // hide cooresponding table
+          $(id).addClass("hide")
+        }
+        else
+        {
+          $(this).addClass("active");                                   // hide cooresponding table
+          id = "#" + this.text.match(/\W*(\w+)/)[1].toLowerCase() + "_table"
+          $(id).removeClass("hide")
+        }
+      })
+    })
+  }
 })
